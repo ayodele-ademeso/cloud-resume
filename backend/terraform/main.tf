a47bb594e334601f03f303f16d7cdb4224ed5536
Ayodele Spencer <ademesoayodele@gmail.com>
2022-03-15 09:00:28 +0100

    created api gateway

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_website_configuration" "config" {
 
 #Create DynamoDB table
 resource "aws_dynamodb_table" "dynamodb" {
-  name         = var.dynamo_db_table
+  name         = "visitor-count"
   hash_key     = "visitorcount"
   billing_mode = "PAY_PER_REQUEST"
 
@@ -46,32 +46,24 @@ resource "aws_dynamodb_table" "dynamodb" {
 
 }
 
-#Create Lambda function
+#Create Lambda role to access DynamoDB
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+resource "aws_iam_role" "role_for_dynamodb" {
+  name               = "myrole"
+  assume_role_policy = file("../assume_role_policy.json")
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
 }
-EOF
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "lambda_policy"
+  role   = aws_iam_role.role_for_dynamodb.id
+  policy = file("../policy.json")
 }
 
 resource "aws_lambda_function" "lambda" {
   filename      = "../lambda_function.zip"
   function_name = "lambda_function"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.role_for_dynamodb.arn
   handler       = "lambda_function.lambda_handler"
 
   source_code_hash = filebase64sha256("../lambda_function.zip")
@@ -80,9 +72,42 @@ resource "aws_lambda_function" "lambda" {
 
   environment {
     variables = {
-      foo = "bar"
+      ddbname = "visitor-count"
     }
   }
 }
 
-#Call API module
+#Create API Gateway
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "CounterAPI"
+  description = "API Gateway to retrieve counter from DynamoDB"
+}
+
+resource "aws_api_gateway_resource" "api" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "count"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.api.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri  = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deploy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+}
