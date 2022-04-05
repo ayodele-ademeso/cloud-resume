50f48ec69bf92b74b141b8c766864097d6c04d37
Ayodele Spencer <ademesoayodele@gmail.com>
2022-04-05 10:04:35 +0100

    Updated lambda code and test

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -9,27 +9,30 @@ def lambda_handler(event,context):
     table = dynamodb.Table(dynamodbTable)
 
     #update value of item atrribute in table by 1 or add attribute if it does not exist
-    dynamodbResponse = table.update_item(
-        Key = {
-            "visitorcount": "count"
-        },
-        #Using 'ADD' to increment value of the attribute 'count' by 1
-        UpdateExpression='ADD counters :inc', #counter is a reserved keyword so I went with counters.
-        ExpressionAttributeValues={
-            ':inc': 1
-        },
-        ReturnValues= "UPDATED_NEW"
-    )
+    try:
+        dynamodbResponse = table.update_item(
+            Key = {
+                "visitorcount": "count"
+            },
+            #Using 'ADD' to increment value of the attribute 'count' by 1
+            UpdateExpression='ADD counters :inc', #counter is a reserved keyword so I went with counters.
+            ExpressionAttributeValues={
+                ':inc': 1
+            },
+            ReturnValues= "UPDATED_NEW"
+        )
+    except ClientError as e:
+        print(e)
+    else:
+        responseBody = json.dumps({"count": int(float(dynamodbResponse["Attributes"]["counters"]))})
 
-    responseBody = json.dumps({"count": int(float(dynamodbResponse["Attributes"]["counters"]))})
-
-    response = {
-        'Status-code': 200,
-        'body': responseBody,
-        'headers': {
-            'content-type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+        response = {
+            'Status-code': 200,
+            'body': responseBody,
+            'headers': {
+                'content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
         }
-    }
 
     return response
--- backend/test_lambda_function.py
+++ backend/test_lambda_function.py
@@ -1,2 +1,61 @@
-import pytest
+import boto3
+import os
+import json
+import unittest
+from contextlib import contextmanager
+from moto import mock_dynamodb2
 import lambda_function
+from unittest import mock
+
+TABLE = 'testdb'
+
+class LambdaTest(unittest.TestCase):
+    """Tests for lambda function"""
+
+    @contextmanager
+    def do_test_setup():
+        with mock_dynamodb2():
+            set_up_dynamodb()
+            yield
+
+
+    def set_up_dynamodb():
+        client = boto3.client('dynamodb', region_name='us-east-1')
+        client.create_table(
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'visitorcount',
+                    'AttributeType': 'S'
+                },
+            ],
+            KeySchema=[
+                {
+                    'AttributeName': 'visitorcount',
+                    'KeyType': 'HASH'
+                }
+            ],
+            TableName=TABLE,
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            }
+        )
+
+
+    @mock.patch.dict(os.environ, {"databaseName": TABLE})
+    def test_handler(*args):
+        #with do_test_setup():
+            # Run call with an event describing the file:
+            response = lambda_function.lambda_handler(None, None)
+
+            # Check that it exists in `processed/`
+            assert response['statusCode'] == 200
+            assert response['body'] == json.dumps({"count": 1})
+
+            response = app.lambda_handler(None, None)
+
+            assert response['statusCode'] == 200
+            assert response['body'] == json.dumps({"count": 2})
+
+if __name__ == '__main__':
+    unittest.main()
