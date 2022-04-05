e167b4f78071c5fd9e8d2f2a0f906bca08eb23af
Ayodele Spencer <ademesoayodele@gmail.com>
2022-04-05 07:59:45 +0100

    API CORS

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -110,6 +110,50 @@ resource "aws_api_gateway_resource" "api" {
   path_part   = "count"
 }
 
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.api.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.api.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+  }
+  depends_on = [aws_api_gateway_method.options_method]
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.api.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  type        = "MOCK"
+  depends_on  = [aws_api_gateway_method.options_method]
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.api.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+  depends_on = [aws_api_gateway_method_response.options_200]
+}
+
 resource "aws_api_gateway_method" "get" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.api.id
@@ -117,19 +161,7 @@ resource "aws_api_gateway_method" "get" {
   authorization = "NONE"
 }
 
-# resource "aws_lambda_permission" "apigw" {
-#   statement_id  = "AllowAPIGatewayInvoke"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.lambda.arn
-#   principal     = "apigateway.amazonaws.com"
-
-#   source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
-# }
-
 resource "aws_api_gateway_integration" "lambda_integration" {
-  # depends_on = [
-  #   aws_lambda_permission.apigw
-  # ]
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_method.get.resource_id
   http_method = aws_api_gateway_method.get.http_method
@@ -178,11 +210,14 @@ resource "aws_api_gateway_method_response" "response_200" {
   http_method = aws_api_gateway_method.get.http_method
   status_code = "200"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = false
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
   }
   response_models = {
     "application/json" = "DynamoDBOutput"
   }
+  depends_on = [aws_api_gateway_method.get]
 }
 
 
@@ -192,7 +227,9 @@ resource "aws_api_gateway_integration_response" "IntegrationResponse" {
   http_method = aws_api_gateway_method.get.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 
   response_templates = {
@@ -202,6 +239,7 @@ resource "aws_api_gateway_integration_response" "IntegrationResponse" {
 $inputRoot.body
   EOF
   }
+  depends_on = [aws_api_gateway_method.get]
 }
 
 resource "aws_api_gateway_deployment" "gateway_deployment" {
