52d6d986ce02a4a9a79490f8f79b0799b4fc8871
Ayodele <ademesoayodele@gmail.com>
2024-01-03 23:09:41 +0000

    updated API url in index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,9 +2,11 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApiUrl = ('https://w2tagdzwhj.execute-api.us-east-1.amazonaws.com/dev/count');
+const functionApiUrl = ('https://w2tagdzwhj.execute-api.us-east-1.amazonaws.com/dev/count/');
 
 const getVisitCount = () => {
+
+    let count;  // Declare count within the scope of getVisitCount
 
     fetch(functionApiUrl)
         .then(response => {
